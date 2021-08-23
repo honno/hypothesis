74215669ee3d45276965e1eccaadded0a724a9c1
Matthew Barber <quitesimplymatt@gmail.com>
2021-08-23 14:37:09 +0100

    Merged narrow float and fill tests into test_arrays.py

--- hypothesis-python/tests/array_api/test_arrays.py
+++ hypothesis-python/tests/array_api/test_arrays.py
@@ -362,3 +362,77 @@ def test_unique_array_without_fill(array):
     # thanks to the 'birthday paradox', but finding unique elemennts is still easy.
     if hasattr(xp, "unique"):
         assume(count_unique(array) == array.size)
+
+
+@given(xps.arrays(xp.bool, (), fill=st.nothing()))
+def test_can_generate_0d_arrays_with_no_fill(array):
+    assert array.dtype == xp.bool
+    assert array.ndim == 0
+    assert array.shape == ()
+
+
+@st.composite
+def distinct_integers(draw):
+    used = draw(st.shared(st.builds(set), key="distinct_integers.used"))
+    i = draw(st.integers(0, 2 ** 64 - 1).filter(lambda x: x not in used))
+    used.add(i)
+    return i
+
+
+@given(xps.arrays(xp.uint64, 10, elements=distinct_integers()))
+def test_does_not_reuse_distinct_integers(array):
+    # xp.unique() is optional for Array API libraries
+    if hasattr(xp, "unique"):
+        unique_values = xp.unique(array)
+        assert unique_values.size == array.size
+
+
+def test_may_reuse_distinct_integers_if_asked():
+    if hasattr(xp, "unique"):
+
+        def nunique(array) -> int:
+            unique_values = xp.unique(array)
+            return unique_values.size
+
+        find_any(
+            xps.arrays(
+                xp.uint64, 10, elements=distinct_integers(), fill=distinct_integers()
+            ),
+            lambda x: nunique(x) < len(x),
+        )
+    else:
+        pytest.skip()
+
+
+def test_minimizes_to_fill():
+    smallest = minimal(xps.arrays(xp.float32, 10, fill=st.just(3.0)))
+    assert xp.all(smallest == 3.0)
+
+
+@given(
+    xps.arrays(
+        dtype=xp.float32,
+        elements=st.floats(width=32).filter(bool),
+        shape=(3, 3, 3),
+        fill=st.just(1.0),
+    )
+)
+def test_fills_everything(array):
+    assert xp.all(array)
+
+
+@pytest.mark.parametrize("dtype", [xp.float32, xp.float64])
+@pytest.mark.parametrize("low", [-2.0, -1.0, 0.0, 1.0])
+@given(st.data())
+def test_bad_float_exclude_min_in_array(dtype, low, data):
+    strat = xps.arrays(
+        dtype=dtype,
+        shape=(),
+        elements={
+            "min_value": low,
+            "max_value": low + 1,
+            "exclude_min": True,
+        },
+    )
+    array = data.draw(strat, label="array")
+    assert array > low
